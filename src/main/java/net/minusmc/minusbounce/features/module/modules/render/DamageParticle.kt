e8cb3e58089cdc56af6bb622ab37833c36457363
Win <win@gmail.com>
2024-08-20 15:09:54 +0700

    Fix DamageParticle

--- src/main/java/net/minusmc/minusbounce/features/module/modules/render/DamageParticle.kt
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/render/DamageParticle.kt
@@ -5,8 +5,6 @@
  */
 package net.minusmc.minusbounce.features.module.modules.render
 
-import net.minecraft.client.renderer.GlStateManager
-import net.minecraft.entity.EntityLivingBase
 import net.minusmc.minusbounce.event.EventTarget
 import net.minusmc.minusbounce.event.Render3DEvent
 import net.minusmc.minusbounce.event.UpdateEvent
@@ -15,57 +13,55 @@ import net.minusmc.minusbounce.features.module.Module
 import net.minusmc.minusbounce.features.module.ModuleCategory
 import net.minusmc.minusbounce.features.module.ModuleInfo
 import net.minusmc.minusbounce.utils.EntityUtils
-import net.minusmc.minusbounce.utils.render.ColorUtils
 import net.minusmc.minusbounce.value.BoolValue
 import net.minusmc.minusbounce.value.IntegerValue
+import net.minecraft.client.renderer.GlStateManager
+import net.minecraft.entity.EntityLivingBase
 import org.lwjgl.opengl.GL11
 import java.awt.Color
 import java.math.BigDecimal
 import java.util.*
 import kotlin.math.abs
 
-@ModuleInfo(name = "DamageParticle", description = "Damage Particle", category = ModuleCategory.RENDER)
+@ModuleInfo(name = "DamageParticle", spacedName = "Damage Particle", description = "Allows you to see targets damage.", category = ModuleCategory.RENDER)
 class DamageParticle : Module() {
+    private val healthData = HashMap<Int, Float>()
+    private val particles = ArrayList<SingleParticle>()
 
-    private val aliveTicksValue = IntegerValue("AliveTicks", 20, 10, 50)
+    private val aliveTicks = IntegerValue("AliveTicks", 20, 10, 50)
     private val sizeValue = IntegerValue("Size", 3, 1, 7)
-    private val colorRedValue = IntegerValue("Red", 68, 0, 255).displayable { !colorRainbowValue.get() }
-    private val colorGreenValue = IntegerValue("Green", 117, 0, 255).displayable { !colorRainbowValue.get() }
-    private val colorBlueValue = IntegerValue("Blue", 255, 0, 255).displayable { !colorRainbowValue.get() }
-    private val colorAlphaValue = IntegerValue("Alpha", 100, 0, 255)
-    private val colorRainbowValue = BoolValue("Rainbow", false)
-
-    private val healthData = mutableMapOf<Int, Float>()
-    private val particles = mutableListOf<SingleParticle>()
+    private val customColor = BoolValue("CustomColor", false)
+    private val red = IntegerValue("Red", 255, 0, 255, { customColor.get() })
+    private val green = IntegerValue("Green", 255, 0, 255, { customColor.get() })
+    private val blue = IntegerValue("Blue", 255, 0, 255, { customColor.get() })
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
         synchronized(particles) {
-            for(entity in mc.theWorld.loadedEntityList) {
-                if(entity is EntityLivingBase && EntityUtils.isSelected(entity,true)) {
-                    val lastHealth = healthData.getOrDefault(entity.entityId,entity.maxHealth)
+            for (entity in mc.theWorld.loadedEntityList) {
+                if (entity is EntityLivingBase && EntityUtils.isSelected(entity, true)) {
+                    val lastHealth = healthData.getOrDefault(entity.entityId, entity.maxHealth)
                     healthData[entity.entityId] = entity.health
-                    if(lastHealth == entity.health) continue
+                    if (lastHealth == entity.health) continue
 
-                    val prefix = if (!colorRainbowValue.get()) (if(lastHealth>entity.health){"§c❤"}else{"§a❤"}) else (if(lastHealth>entity.health){"-"}else{"+"})
-                    particles.add(SingleParticle(prefix + BigDecimal(abs(lastHealth - entity.health).toDouble()).setScale(1, BigDecimal.ROUND_HALF_UP).toDouble()
-                        ,entity.posX - 0.5 + Random(System.currentTimeMillis()).nextInt(5).toDouble() * 0.1
-                        ,entity.entityBoundingBox.minY + (entity.entityBoundingBox.maxY - entity.entityBoundingBox.minY) / 2.0
-                        ,entity.posZ - 0.5 + Random(System.currentTimeMillis() + 1L).nextInt(5).toDouble() * 0.1)
+                    val prefix = if (!customColor.get()) (if (lastHealth > entity.health) "§c" else "§a") else (if (lastHealth > entity.health) "-" else "+")
+                    particles.add(SingleParticle(prefix + BigDecimal(abs(lastHealth - entity.health).toDouble()).setScale(1, BigDecimal.ROUND_HALF_UP).toDouble(),
+                        entity.posX - 0.5 + Random(System.currentTimeMillis()).nextInt(5).toDouble() * 0.1,
+                        entity.entityBoundingBox.minY + (entity.entityBoundingBox.maxY - entity.entityBoundingBox.minY) / 2.0,
+                        entity.posZ - 0.5 + Random(System.currentTimeMillis() + 1L).nextInt(5).toDouble() * 0.1)
                     )
                 }
             }
 
-            val needRemove = ArrayList<SingleParticle> ()
+            val needRemove = ArrayList<SingleParticle>()
             for (particle in particles) {
                 particle.ticks++
-                if (particle.ticks>aliveTicksValue.get()) {
+                if (particle.ticks>aliveTicks.get())
                     needRemove.add(particle)
-                }
             }
-            for (particle in needRemove) {
+
+            for (particle in needRemove)
                 particles.remove(particle)
-            }
         }
     }
 
@@ -76,15 +72,15 @@ class DamageParticle : Module() {
             val size = sizeValue.get() * 0.01
 
             for (particle in particles) {
-                val n: Double = particle.posX - renderManager.renderPosX
-                val n2: Double = particle.posY - renderManager.renderPosY
-                val n3: Double = particle.posZ - renderManager.renderPosZ
+                val n = particle.posX - renderManager.renderPosX
+                val n2 = particle.posY - renderManager.renderPosY
+                val n3 = particle.posZ - renderManager.renderPosZ
                 GlStateManager.pushMatrix()
                 GlStateManager.enablePolygonOffset()
                 GlStateManager.doPolygonOffset(1.0f, -1500000.0f)
                 GlStateManager.translate(n.toFloat(), n2.toFloat(), n3.toFloat())
                 GlStateManager.rotate(-renderManager.playerViewY, 0.0f, 1.0f, 0.0f)
-                val textY = if (mc.gameSettings.thirdPersonView == 2) { -1.0f } else { 1.0f }
+                val textY = if (mc.gameSettings.thirdPersonView == 2) -1.0f else 1.0f
 
                 GlStateManager.rotate(renderManager.playerViewX, textY, 0.0f, 0.0f)
                 GlStateManager.scale(-size, -size, size)
@@ -93,13 +89,12 @@ class DamageParticle : Module() {
                     particle.str,
                     (-(mc.fontRendererObj.getStringWidth(particle.str) / 2)).toFloat(),
                     (-(mc.fontRendererObj.FONT_HEIGHT - 1)).toFloat(),
-                    (if (colorRainbowValue.get()) ColorUtils.rainbowWithAlpha(colorAlphaValue.get()) else Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get(), colorAlphaValue.get())).rgb
+                    if (customColor.get()) Color(red.get(), green.get(), blue.get()).rgb else 0
                 )
                 GL11.glColor4f(187.0f, 255.0f, 255.0f, 1.0f)
                 GL11.glDepthMask(true)
                 GlStateManager.doPolygonOffset(1.0f, 1500000.0f)
                 GlStateManager.disablePolygonOffset()
-                GlStateManager.resetColor()
                 GlStateManager.popMatrix()
             }
         }
--- src/main/java/net/minusmc/minusbounce/utils/render/ColorUtils.kt
+++ src/main/java/net/minusmc/minusbounce/utils/render/ColorUtils.kt
@@ -104,22 +104,6 @@ object ColorUtils {
     }
 
     @JvmStatic
-    fun hslRainbow(
-        index: Int,
-        lowest: Float = HUD.rainbowStartValue.get(),
-        bigest: Float = HUD.rainbowStopValue.get(),
-        indexOffset: Int = 300,
-        timeSplit: Int = HUD.rainbowSpeedValue.get(),
-        saturation: Float = HUD.rainbowSaturationValue.get(),
-        brightness: Float = HUD.rainbowBrightnessValue.get()
-    ): Color {
-        return Color.getHSBColor((abs(((((System.currentTimeMillis() - startTime).toInt() + index * indexOffset) / timeSplit.toFloat()) % 2) - 1) * (bigest - lowest)) + lowest, saturation, brightness)
-    }
-
-    @JvmStatic
-    fun rainbowWithAlpha(alpha: Int) = reAlpha(hslRainbow(1), alpha)
-
-    @JvmStatic
     fun rainbow(alpha: Float) = rainbow(400000L, alpha)
 
     @JvmStatic
