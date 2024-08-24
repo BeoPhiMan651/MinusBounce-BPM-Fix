9518709444975cd03c1e6ec9e75bcd7720a4a615
Win <win@gmail.com>
2024-08-24 07:37:02 +0700

    Fix LegitSpeed

--- src/main/java/net/minusmc/minusbounce/features/module/modules/movement/speeds/other/LegitSpeed.kt
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/movement/speeds/other/LegitSpeed.kt
@@ -5,37 +5,23 @@
  */
 package net.minusmc.minusbounce.features.module.modules.movement.speeds.other
 
+import net.minusmc.minusbounce.utils.player.MovementUtils.isMoving
 import net.minusmc.minusbounce.features.module.modules.movement.speeds.SpeedMode
-import net.minusmc.minusbounce.utils.player.MovementUtils
-import net.minusmc.minusbounce.value.BoolValue
-import net.minecraft.client.settings.GameSettings
 import net.minusmc.minusbounce.features.module.modules.movement.speeds.SpeedType
+import net.minusmc.minusbounce.utils.extensions.tryJump
 
 class LegitSpeed: SpeedMode("Legit", SpeedType.OTHER) {
+    fun onStrafe() {
+        val player = mc.thePlayer ?: return
 
-    private val cpuSPEED = BoolValue("Legit-AlanWOOD-CPU-BYPASS-$$$$", true)
-
-    private var wasOnGround = false
-
-    override fun onUpdate() {
-        if (cpuSPEED.get()) mc.timer.timerSpeed = 1.004f
-        if (MovementUtils.isMoving) {
-            if (mc.thePlayer.onGround) {
-                mc.gameSettings.keyBindJump.pressed = true
-                wasOnGround = true
-            } else {
-                if (wasOnGround) {
-                    mc.gameSettings.keyBindJump.pressed = false
-                    wasOnGround = false
-                } else {
-                    mc.gameSettings.keyBindJump.pressed = true
-                }
-            }
+        if (mc.thePlayer.onGround && isMoving) {
+            player.tryJump()
         }
     }
 
-    override fun onDisable() {
-        mc.gameSettings.keyBindJump.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindJump)
-        if (cpuSPEED.get()) mc.timer.timerSpeed = 1f
+    override fun onUpdate() {
+        val player = mc.thePlayer ?: return
+
+        player.isSprinting = player.movementInput.moveForward > 0.8
     }
 }
